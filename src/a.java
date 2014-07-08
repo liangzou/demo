579f243e27ebe9da12ce48759949f587c9a6fa7d
Administrator <Administrator@leon>
2014-07-08 23:46:36 +0800

    gfew

--- src/a.java
+++ src/a.java
@@ -1,5 +1,36 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class a {
-	public void run() {
-          System.out.println("fwefwe");
+	transient int tt=3;
+	public void run() throws IOException {
+		  
+       //   System.out.println("fwefwe"+tt);
+		//BufferedReader br=new BufferedReader(new FileReader("D:\\Music\\bigbang - blue.mp3"));
+		BufferedInputStream r=new BufferedInputStream(new FileInputStream(new File("D:\\Music\\bigbang - blue.mp3")));
+      //   FileOutputStream ts=new FileOutputStream(new File("C:\\Users\\Administrator\\Desktop\\mp3.mp3"));
+		//BufferedWriter bw=new BufferedWriter(new FileWriter("C:\\Users\\Administrator\\Desktop\\mp13.mp3"));
+		BufferedOutputStream bw=new BufferedOutputStream(new FileOutputStream(new File("C:\\Users\\Administrator\\Desktop\\mp3.mp3")));
+         //ts.write(r.read());
+         int t=0;
+        // byte[] buffer=new byte[1024];
+        // r.r
+         while((t=r.read())!=-1){
+        	 bw.write( t);
+        	// System.out.println(r.read());
+         }
+     //   r.r
+        
+	}
+	
+	
+	public static void main(String[] args) throws IOException{
+		a as=new a();
+		as.run();
+		
 	}
 }
